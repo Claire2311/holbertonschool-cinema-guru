aad6b755960abd8c48aa10dc62da19ed85390005
Claire2311 <claire.girard@ieseg.fr>
2025-05-16 12:58:59 +0200

    create input select input

---
+++ src/components/general/SelectInput.js
@@ -0,0 +1,28 @@
+import "./general.css";
+
+function SelectInput({ label, options, className, value, setValue }) {
+  function handleSelect(onChange) {
+    setValue(onChange.target.value);
+  }
+
+  return (
+    <>
+      <label for={label}>
+        {label}
+
+        <select
+          name={label}
+          id={label}
+          className={className}
+          onChange={handleSelect}
+        >
+          {options.map((opt) => (
+            <option value={opt}></option>
+          ))}
+        </select>
+      </label>
+    </>
+  );
+}
+
+export default SelectInput;
